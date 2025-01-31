abaaa84d5457443c10fb61b85cba5001b4529864
peppermint100 <krpeppermint100@gmail.com>
2025-01-31 09:22:30 +0900

    Update ShortenUrlIdGenerator.kt

--- url-shortner/src/main/kotlin/com/example/urlshortner/entity/ShortenUrlIdGenerator.kt
+++ url-shortner/src/main/kotlin/com/example/urlshortner/entity/ShortenUrlIdGenerator.kt
@@ -6,22 +6,23 @@ import org.springframework.stereotype.Component
 
 @Component
 class ShortenUrlIdGenerator(
-    @Value("\${server.id}") private val instanceId: Int,
+    @Value("spring.application.name") private val applicationName: String,
+    @Value("\${server.id}") private val serverId: Int,
     private val redisTemplate: RedisTemplate<String, String>
 ) {
     companion object {
-        private const val SEQUENCE_KEY = "url:sequence"
         private const val MAX_SEQUENCE = 1000
     }
 
     fun nextId(): String {
+        val sequenceKey = "${applicationName}:${serverId}"
         // 분산 환경에서도 유니크 하도록 Redis를 이용한 시퀀스 생성
         val sequence = (redisTemplate.opsForValue()
-            .increment(SEQUENCE_KEY) ?: 0) % MAX_SEQUENCE
+            .increment(sequenceKey) ?: 0) % MAX_SEQUENCE
 
         return buildString {
             append(System.currentTimeMillis())
-            append(instanceId.toString().padStart(2, '0'))
+            append(serverId.toString().padStart(2, '0'))
             append(sequence.toString().padStart(3, '0'))
         }
     }
