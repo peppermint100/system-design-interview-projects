6f961043b6e9d7ae716d65abca50774ae088c838
peppermint100 <krpeppermint100@gmail.com>
2025-02-01 18:46:22 +0900

    Fanout 서비스에서 감지한 포스트를 뉴스피드 캐시에 추가

--- fanout-service/src/main/kotlin/com/example/fanoutservice/kafka/CreatePostEventConsumer.kt
+++ fanout-service/src/main/kotlin/com/example/fanoutservice/kafka/CreatePostEventConsumer.kt
@@ -1,12 +1,15 @@
 package com.example.fanoutservice.kafka
 
+import com.example.fanoutservice.service.NewsfeedCacheService
 import com.example.fanoutservice.vo.CreatePostEvent
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
-class CreatePostEventConsumer {
+class CreatePostEventConsumer(
+    private val newsfeedCacheService: NewsfeedCacheService
+) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     companion object {
@@ -21,6 +24,7 @@ class CreatePostEventConsumer {
     fun consumeCreatePostEvent(event: CreatePostEvent) {
         try {
             logger.info("포스트 생성 이벤트를 감지했습니다 ${event.postId}, ${event.userId}")
+            newsfeedCacheService.cachePostToNewsfeed(event.userId, event.postId)
         } catch (e: Exception) {
             logger.error("포스트 생성 이벤트를 소비하는데 실패했습니다 ${event.postId}")
         }
