4414642101b50e86469b67ff55a32cc9807c606e
peppermint100 <krpeppermint100@gmail.com>
2025-02-05 13:54:50 +0900

    BlockStorageService 프로젝트 생성

---
+++ google-drive/block-storage-service/src/test/kotlin/com/example/blockstorageservice/BlockStorageServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.blockstorageservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlockStorageServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
